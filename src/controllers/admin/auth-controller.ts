e9f2367bfd4261ded45be8b1099c0252bd19596c
Ifham_baig <125736619+ifham001@users.noreply.github.com>
2025-09-05 23:56:24 +0530

    Update auth-controller.ts

--- src/controllers/admin/auth-controller.ts
+++ src/controllers/admin/auth-controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
-import adminAuthSchema from '../../models/admin/adminSchemas'
+import adminAuthSchema from '../../models/admin/Admin'
 import HttpError from '../../models/Http-Error';
 import bcrypt from 'bcryptjs';
 import { IAdmin } from '../../types/admin.types';
